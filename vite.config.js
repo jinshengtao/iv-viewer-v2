1d7c14218a817cc549814a2e4901fe12ed629087
ZENG JINLONG <jalonzeng@gmail.com>
2024-12-25 11:43:34 +0800

    修复图片无法使用本地url的bug

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-import { resolve } from 'path'
 export default defineConfig({
   plugins: [vue(), cssInjectedByJsPlugin()],
   build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'IVViewerV2',
+      fileName: (format) => `iv-viewer-v2.${format}.js`,
+    },
     rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.js'),
-        demo: resolve(__dirname, 'demo/index.html'),
-      },
       output: {
         globals: {
           vue: 'Vue',
         },
       },
       external: ['vue'],
-      lib: {
-        entry: 'src/index.js',
-        name: 'IVViewerV2',
-        fileName: (format) => `iv-viewer-v2.${format}.js`,
-      },
     },
   },
 })
